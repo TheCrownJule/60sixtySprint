ebc35a06c73140f423d8cdb6a649f120933e2421
Julia Fae Wilson <112116529+TheCrownJule@users.noreply.github.com>
2023-11-30 18:27:56 +0200

    Update SceneManage.cs

--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneManage : MonoBehaviour
 {
+    //build index is the order of the scenes in the build settings
+    //build index starts at 0
+
     public void LoadLevelOne()
     {
         //load scene 3 in build index
